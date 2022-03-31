e08af54fbeeca2c17eff39742ac02117bad5a1ac
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-03-31 12:28:18 +0800

    Updated module version.

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "static-website" {
   source         = "app.terraform.io/fer1035/static-website/aws"
-  version        = "1.0.3"
+  version        = "1.0.4"
   # insert required variables here
   tag_Name       = "VCS Test"
   tag_sitecode   = "123"
