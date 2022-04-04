6f853e245c20d0d993ab5d97ba15e1f2607611d6
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-04-04 16:04:53 +0800

    Removed module provider blocks.

--- main.tf
+++ main.tf
@@ -5,67 +5,28 @@ data "aws_caller_identity" "current" {}
 locals {
   account_id = data.aws_caller_identity.current.account_id
 }
-variable "region" {
-  type        = string
-  default     = "us-east-1"
-  description = "Working region."
-}
-provider "aws" {
-  region = var.region
-}
 
-# Required tags data.
-variable "tag_Name" {
-  type        = string
-  default     = "VCS Test"
-  description = "Human-friendly name for the project resources."
-}
-variable "tag_sitecode" {
-  type        = string
-  default     = "123"
-  description = "Which site the project is attached to."
-}
-variable "tag_department" {
-  type        = string
-  default     = "Demolition"
-  description = "Which department owns the project."
-}
-variable "tag_team" {
-  type        = string
-  default     = "myteam@mycompany.com"
-  description = "Which team owns the project."
-}
-variable "tag_tier" {
-  type        = string
-  default     = "dev"
-  description = "Deployment tier for the resources."
-}
-variable "tag_costcenter" {
-  type        = string
-  default     = "1234"
-  description = "Which cost center the project is attached to."
+# Provider data.
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Name       = "VCS Test"
+      sitecode   = "123"
+      department = "Demolition"
+      team       = "myteam@mycompany.com"
+      tier       = "dev"
+      costcenter = "1234"
+    }
+  }
 }
 
 # Modules.
 module "static-website" {
   source         = "app.terraform.io/fer1035/static-website/aws"
-  region         = var.region
-  tag_Name       = var.tag_Name
-  tag_sitecode   = var.tag_sitecode
-  tag_department = var.tag_department
-  tag_team       = var.tag_team
-  tag_tier       = var.tag_tier
-  tag_costcenter = var.tag_costcenter
 }
 module "cloudfront-invalidator" {
   source         = "app.terraform.io/fer1035/s3-cloudfront-invalidator/aws"
-  region         = var.region
-  tag_Name       = var.tag_Name
-  tag_sitecode   = var.tag_sitecode
-  tag_department = var.tag_department
-  tag_team       = var.tag_team
-  tag_tier       = var.tag_tier
-  tag_costcenter = var.tag_costcenter
   bucket_name    = module.static-website.s3_bucket_name
   bucket_arn     = module.static-website.s3_bucket_arn
   cloudfront_id  = module.static-website.cloudfront_id
@@ -73,13 +34,6 @@ module "cloudfront-invalidator" {
 }
 /* module "network" {
   source                = "app.terraform.io/fer1035/network/aws"
-  region                = var.region
-  tag_Name              = var.tag_Name
-  tag_sitecode          = var.tag_sitecode
-  tag_department        = var.tag_department
-  tag_team              = var.tag_team
-  tag_tier              = var.tag_tier
-  tag_costcenter        = var.tag_costcenter
   vpc_cidr              = "10.0.0.0/16"
   subnet_public_1_cidr  = "10.0.0.0/24"
   subnet_private_1_cidr = "10.0.1.0/24"
@@ -87,13 +41,6 @@ module "cloudfront-invalidator" {
 }
 module "security-group" {
   source           = "app.terraform.io/fer1035/security-group/aws"
-  region           = var.region
-  tag_Name         = var.tag_Name
-  tag_sitecode     = var.tag_sitecode
-  tag_department   = var.tag_department
-  tag_team         = var.tag_team
-  tag_tier         = var.tag_tier
-  tag_costcenter   = var.tag_costcenter
   ingress_from     = 80
   ingress_to       = 80
   ingress_protocol = "tcp"
