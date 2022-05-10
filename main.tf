b9fcbaec93207e5ae17f93efb10b576b83ce3efb
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-05-10 14:12:17 +0800

    Added an extra EC2 instance to infra.

--- main.tf
+++ main.tf
@@ -97,14 +97,26 @@ module "security-group" {
   vpc_id           = module.network.vpc_id
 }
 
-module "ec2-instance" {
+module "ec2-instance-1" {
   source          = "app.terraform.io/fer1035/ec2-instance/aws"
   subnet          = module.network.public_subnet_1
   security_groups = [module.security-group.security_group_id]
   public_key      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDN5A0ppqf2Dj6b7w9VgBiIR3/KwUONb2oUI55IFjU5S1An2j7jPgaoxGjZDhLuU5OEmXnuG7llJGor+3/uwV2lhZ/WtNpI2PopbjZVAWv4nPX4SRWPZ1uYabtydbeUHyHD+FXRfRA+hHsFlYyUdyif9QyNj7U/lM3I9E9igf6j2aUfbJiizTsGpabOxd14Hw7fXRNP8gF1nhw6ek4w3js4nWgY6nmSnQVeelOIU1zNzi35bRL0mJTpEQBJMJP1+0nd1Xl9g7J8f9w6cFCZNk5X16SDlyApusiLSkO5HmmLaKF+jDbMhs8bVOkLHvt3ScmNs2hvGwXCTr40qwDpky7ATvoShNHZJPImUSOz9arcfP5wrar4MHhonM5L3vmmcS6yB4bM0yIpcTM+IInsHf5nvs2i71BKj+opMtTEin/lUxdZauu/G4a/Kctz8pQAZScAV4KmzF9e/XqdvXZJahoFFalf6yXSg2bPYbgjfuIiuOwSMkKgN1iNIoa0DLnqTFs= abdahmad@Ahmads-MacBook-Pro.local"
 }
 
-output "instance_private_dns" {
-  value       = module.ec2-instance.private_dns
+module "ec2-instance-2" {
+  source          = "app.terraform.io/fer1035/ec2-instance/aws"
+  subnet          = module.network.public_subnet_1
+  security_groups = [module.security-group.security_group_id]
+  public_key      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDN5A0ppqf2Dj6b7w9VgBiIR3/KwUONb2oUI55IFjU5S1An2j7jPgaoxGjZDhLuU5OEmXnuG7llJGor+3/uwV2lhZ/WtNpI2PopbjZVAWv4nPX4SRWPZ1uYabtydbeUHyHD+FXRfRA+hHsFlYyUdyif9QyNj7U/lM3I9E9igf6j2aUfbJiizTsGpabOxd14Hw7fXRNP8gF1nhw6ek4w3js4nWgY6nmSnQVeelOIU1zNzi35bRL0mJTpEQBJMJP1+0nd1Xl9g7J8f9w6cFCZNk5X16SDlyApusiLSkO5HmmLaKF+jDbMhs8bVOkLHvt3ScmNs2hvGwXCTr40qwDpky7ATvoShNHZJPImUSOz9arcfP5wrar4MHhonM5L3vmmcS6yB4bM0yIpcTM+IInsHf5nvs2i71BKj+opMtTEin/lUxdZauu/G4a/Kctz8pQAZScAV4KmzF9e/XqdvXZJahoFFalf6yXSg2bPYbgjfuIiuOwSMkKgN1iNIoa0DLnqTFs= abdahmad@Ahmads-MacBook-Pro.local"
+}
+
+output "instance_1_private_dns" {
+  value       = module.ec2-instance-1.private_dns
+  description = "Private DNS address for the EC2 instance."
+}
+
+output "instance_2_private_dns" {
+  value       = module.ec2-instance-2.private_dns
   description = "Private DNS address for the EC2 instance."
 }
